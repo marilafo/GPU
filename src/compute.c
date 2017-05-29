e45135294eba980e1823b17f186f234b36449ccf
Marion Lafon <marilafo@emi.u-bordeaux1.fr>
2017-05-29 16:00:45 +0200

    modifs fonctions

--- src/compute.c
+++ src/compute.c
@@ -77,14 +77,8 @@ unsigned opencl_used [] = {
 };
 
 ///////////////////////////// Version séquentielle simple
-
-
-unsigned compute_v0 (unsigned nb_iter)
-{
-  int alive = 0;
-  for (unsigned test = 1; test <= nb_iter; test++) {
-    for (int i = 1; i < DIM-1; i++)
-      for (int j = 1; j < DIM-1; j++){
+void calcul_vie(int i, int j){
+	int alive = 0;
 	alive = (cur_img(i-1, j-1) !=0)
 	  + (cur_img(i-1, j) != 0)
 	  + (cur_img(i+1, j-1) != 0)
@@ -107,19 +101,23 @@ unsigned compute_v0 (unsigned nb_iter)
 	    next_img(i,j) = 0;
 	}
 	
-
 	alive = 0;
-	
-      }
-      
-    
+}
+
+unsigned compute_v0 (unsigned nb_iter)
+{
+  for (unsigned test = 1; test <= nb_iter; test++) {
+    for (int i = 1; i < DIM-1; i++)
+      for (int j = 1; j < DIM-1; j++){
+		calcul_vie(i,j);
+      } 
     swap_images ();
     }
   return 0;
 }
 
 
-///////////////////////////// Version OpenMP de base
+///////////////////////////// Version OpenMP for simple
 
 void first_touch_v1 ()
 {
@@ -131,46 +129,14 @@ unsigned compute_v1(unsigned nb_iter)
 {
 	#pragma omp parallel
 	{
-
 		#pragma omp for
 	  	for (unsigned test = 1; test <= nb_iter; test++) {
-
-
-		   	int alive = 0;
-		   	int test;
 		   	#pragma omp parallel for collapse(2)
 		   	for (int i = 1; i < DIM-1; i++)
 		   		for (int j = 1; j < DIM-1; j++){
-					alive = (cur_img(i-1, j-1) !=0)
-				  	+ (cur_img(i-1, j) != 0)
-				  	+ (cur_img(i+1, j-1) != 0)
-				  	+ (cur_img(i, j-1) != 0)
-				  	+ (cur_img(i, j+1) != 0)
-				  	+ (cur_img(i-1, j+1) != 0)
-				  	+ (cur_img(i+1, j) != 0)
-				  	+ (cur_img(i+1, j+1) !=0);
-
-					test = cur_img(i, j);
-
-					if(test != 0){
-
-				  	if ((alive == 2) || (alive == 3))
-				    	next_img(i,j) = 0xFFFF00FF;
-				  	else
-				    	next_img(i,j) = 0;
-					}
-					else{
-				  		if(alive == 3)
-				    		next_img(i,j) = 0xFFFF00FF;
-				  	else
-				    	next_img(i,j) = 0;
-					}
-				
-
-					alive = 0;
+					calcul_vie(i,j);
 		
 	   		}
-      
     	}
     }
     swap_images ();
@@ -194,110 +160,50 @@ unsigned compute_v2(unsigned nb_iter)
 
 		#pragma omp for
 	  	for (unsigned test = 1; test <= nb_iter; test++) {
-
-
-		   	int alive = 0;
-		   	int test;
 		   	#pragma omp parallel for 
 		   	for (int i = 1; i < DIM-1; i++)
 		   		#pragma omp parallel for
 		   		for (int j = 1; j < DIM-1; j++){
-					alive = (cur_img(i-1, j-1) !=0)
-				  	+ (cur_img(i-1, j) != 0)
-				  	+ (cur_img(i+1, j-1) != 0)
-				  	+ (cur_img(i, j-1) != 0)
-				  	+ (cur_img(i, j+1) != 0)
-				  	+ (cur_img(i-1, j+1) != 0)
-				  	+ (cur_img(i+1, j) != 0)
-				  	+ (cur_img(i+1, j+1) !=0);
-
-					test = cur_img(i, j);
-
-					if(test != 0){
-
-				  	if ((alive == 2) || (alive == 3))
-				    	next_img(i,j) = 0xFFFF00FF;
-				  	else
-				    	next_img(i,j) = 0;
-					}
-					else{
-				  		if(alive == 3)
-				    		next_img(i,j) = 0xFFFF00FF;
-				  	else
-				    	next_img(i,j) = 0;
-					}
-				
-
-					alive = 0;
-		
+					calcul_vie(i,j);
 	   		}
-      
     	}
     }
     swap_images ();
   return 0; 
 }
 
-
-
-
 ///////////////////////////// Version séquentielle avec tuile
 #define GRAIN 16
 unsigned tranche = 0;
 
-
 volatile int cont = 0;
 
-unsigned jeu_vie_seq (int i_d, int j_d, int i_f, int j_f)
+void get_tuile(int *ret, int i, int j){
+	ret[0] = (i == 0) ? 1 : i * tranche;
+  	ret[1] = (j == 0) ? 1 : j * tranche;
+  	ret[2] = (i == GRAIN-1) ? DIM-2 : (i+1) * tranche - 1;
+  	ret[3] = (j == GRAIN-1) ? DIM-2 : (j+1) * tranche - 1;
+
+  PRINT_DEBUG ('c', "Descente: bloc(%d,%d) couvre (%d,%d)-(%d,%d)\n", i, j, ret[0], ret[1], ret[2], ret[3]);
+  
+}
+
+
+unsigned jeu_vie_seq (int a, int b)
 {
    	{
 
-  	int alive = 0;
-    for (int i = i_d; i <= i_f; i++)
-	    for (int j = j_d; j <= j_f; j++){
-			alive = (cur_img(i-1, j-1) !=0)
-			  + (cur_img(i-1, j) != 0)
-			  + (cur_img(i+1, j-1) != 0)
-			  + (cur_img(i, j-1) != 0)
-			  + (cur_img(i, j+1) != 0)
-			  + (cur_img(i-1, j+1) != 0)
-			  + (cur_img(i+1, j) != 0)
-			  + (cur_img(i+1, j+1) !=0);
+	   	int ret[4];
+	   	get_tuile(ret, a, b);
 
-			if(cur_img(i,j) != 0){
-			  	if ((alive == 2) || (alive == 3))
-			    	next_img(i,j) = 0xFFFF00FF;
-			  	else
-			    	next_img(i,j) = 0;
-			}
-			else{
-			  	if(alive == 3)
-			    	next_img(i,j) = 0xFFFF00FF;
-			  	else
-			    	next_img(i,j) = 0;
-			}
-		
-
-			alive = 0;
-			
-	    }
+    	for (int i = ret[0]; i <= ret[2]; i++)
+		    for (int j = ret[1]; j <= ret[3]; j++){
+				calcul_vie(i,j);
+		    }
 	}
-    
   	return 0;
 }
 
-void lancer_jeu_seq (int i, int j)
-{
-  int i_d = (i == 0) ? 1 : i * tranche;
-  int j_d = (j == 0) ? 1 : j * tranche;
-  int i_f = (i == GRAIN-1) ? DIM-2 : (i+1) * tranche - 1;
-  int j_f = (j == GRAIN-1) ? DIM-2 : (j+1) * tranche - 1;
-
-  PRINT_DEBUG ('c', "Descente: bloc(%d,%d) couvre (%d,%d)-(%d,%d)\n", i, j, i_d, j_d, i_f, j_f);
-  
-  if (jeu_vie_seq (i_d, j_d, i_f, j_f))
-    cont = 1;
-}
 
 // Renvoie le nombre d'itérations effectuées avant stabilisation, ou 0
 unsigned compute_v3 (unsigned nb_iter)
@@ -308,68 +214,34 @@ unsigned compute_v3 (unsigned nb_iter)
   		cont = 0;
   		for (int i=0; i < GRAIN; i++)
     		for (int j=0; j < GRAIN; j++){
-
-      			lancer_jeu_seq (i, j);
+      			jeu_vie_seq (i, j);
     		}
 	}
    	swap_images ();
-
   return cont;
 }
 
-//////////////////////////version tuiléé séquentielle
-unsigned jeu_vie_v4 (int i_d, int j_d, int i_f, int j_f)
+//////////////////////////version OMP for tuilé
+unsigned jeu_vie_v4 (int a, int b)
 {
+
+	int ret[4];
+   	get_tuile(ret, a, b);
+   	
    	#pragma omp parallel
    	{
+   		
 
-  	int alive = 0;
-  	#pragma omp for collapse(2)
-    for (int i = i_d; i <= i_f; i++)
-	    for (int j = j_d; j <= j_f; j++){
-			alive = (cur_img(i-1, j-1) !=0)
-			  + (cur_img(i-1, j) != 0)
-			  + (cur_img(i+1, j-1) != 0)
-			  + (cur_img(i, j-1) != 0)
-			  + (cur_img(i, j+1) != 0)
-			  + (cur_img(i-1, j+1) != 0)
-			  + (cur_img(i+1, j) != 0)
-			  + (cur_img(i+1, j+1) !=0);
-
-			if(cur_img(i,j) != 0){
-			  	if ((alive == 2) || (alive == 3))
-			    	next_img(i,j) = 0xFFFF00FF;
-			  	else
-			    	next_img(i,j) = 0;
-			}
-			else{
-			  	if(alive == 3)
-			    	next_img(i,j) = 0xFFFF00FF;
-			  	else
-			    	next_img(i,j) = 0;
-			}
-		
-
-			alive = 0;
+  		#pragma omp for collapse(2)
+    	for (int i = ret[0]; i <= ret[2]; i++)
+	    	for (int j = ret[1]; j <= ret[3]; j++){
+				calcul_vie(i,j);
 			
 	    }
-	}
-    
+	} 
   	return 0;
 }
 
-void lancer_jeu_v4 (int i, int j)
-{
-  int i_d = (i == 0) ? 1 : i * tranche;
-  int j_d = (j == 0) ? 1 : j * tranche;
-  int i_f = (i == GRAIN-1) ? DIM-2 : (i+1) * tranche - 1;
-  int j_f = (j == GRAIN-1) ? DIM-2 : (j+1) * tranche - 1;
-
-  PRINT_DEBUG ('c', "Descente: bloc(%d,%d) couvre (%d,%d)-(%d,%d)\n", i, j, i_d, j_d, i_f, j_f);
-  
-  if (jeu_vie_v4 (i_d, j_d, i_f, j_f))
-    cont = 1;
-}
 
 // Renvoie le nombre d'itérations effectuées avant stabilisation, ou 0
 unsigned compute_v4 (unsigned nb_iter)
@@ -383,7 +255,7 @@ unsigned compute_v4 (unsigned nb_iter)
   		#pragma omp parallel for collapse(2)
   		for (int i=0; i < GRAIN; i++)
     		for (int j=0; j < GRAIN; j++){
-      			lancer_jeu_v4 (i, j);
+      			jeu_vie_v4 (i, j);
     		}
 	}
    	swap_images ();
@@ -398,66 +270,27 @@ volatile int test_matrice;
 volatile int cellule[GRAIN][GRAIN+1];
 
 
-unsigned jeu_vie_v5 (int i_d, int j_d, int i_f, int j_f)
+unsigned jeu_vie_v5 (int a, int b)
 {
    	{
+   		int ret[4];
+   		get_tuile(ret, a, b);
 
-  	int alive = 0;
-    for (int i = i_d; i <= i_f; i++)
-	    for (int j = j_d; j <= j_f; j++){
-			alive = (cur_img(i-1, j-1) !=0)
-			  + (cur_img(i-1, j) != 0)
-			  + (cur_img(i+1, j-1) != 0)
-			  + (cur_img(i, j-1) != 0)
-			  + (cur_img(i, j+1) != 0)
-			  + (cur_img(i-1, j+1) != 0)
-			  + (cur_img(i+1, j) != 0)
-			  + (cur_img(i+1, j+1) !=0);
-
-			if(cur_img(i,j) != 0){
-			  	if ((alive == 2) || (alive == 3)){
-			    	next_img(i,j) = 0xFFFF00FF;
-			    	test_matrice = 1;
-			  	}
-			  	else
-			    	next_img(i,j) = 0;
-			}
-			else{
-			  	if(alive == 3)
-			    	next_img(i,j) = 0xFFFF00FF;
-			  	else{
-			    	next_img(i,j) = 0;
-			  		test_matrice = 1;
-			  	}
-			}
-		
-
-			alive = 0;
+    	for (int i = ret[0]; i <= ret[2]; i++)
+		    for (int j = ret[1]; j <= ret[3]; j++){
+				calcul_vie(i,j);
 			
 	    }
 	}
+
+	if(test_matrice == 1)
+	    cellule[a][b] = 1;
+	else
+	    cellule[a][b] = 0;
     
   	return 0;
 }
 
-void lancer_jeu_v5 (int i, int j)
-{
-  	int i_d = (i == 0) ? 1 : i * tranche;
-  	int j_d = (j == 0) ? 1 : j * tranche;
-  	int i_f = (i == GRAIN-1) ? DIM-2 : (i+1) * tranche - 1;
-  	int j_f = (j == GRAIN-1) ? DIM-2 : (j+1) * tranche - 1;
-
-  	PRINT_DEBUG ('c', "Descente: bloc(%d,%d) couvre (%d,%d)-(%d,%d)\n", i, j, i_d, j_d, i_f, j_f);
-  
-  	if (jeu_vie_v5 (i_d, j_d, i_f, j_f))
-    	cont = 1;
-
-	if(test_matrice == 1){
-    	cellule[i][j] = 1;
-    }
-    else
-    	cellule[i][j] = 0;
-}
 
 // Renvoie le nombre d'itérations effectuées avant stabilisation, ou 0
 unsigned compute_v5 (unsigned nb_iter)
@@ -485,7 +318,7 @@ unsigned compute_v5 (unsigned nb_iter)
 	    			&& cellule[i+1][j+1] == 0 )
 	    			continue;
 	    		test_matrice = 0;
-      			lancer_jeu_v5 (i, j);
+      			jeu_vie_v5 (i, j);
     		}
 	}
    	swap_images ();
@@ -500,68 +333,27 @@ volatile int test_matrice;
 volatile int cellule[GRAIN][GRAIN+1];
 
 
-unsigned jeu_vie_v6 (int i_d, int j_d, int i_f, int j_f)
+unsigned jeu_vie_v6 (int a, int b)
 {
+	int ret[4];
+   	get_tuile(ret, a, b);
+
 	#pragma omp parallel
    	{
-
-  	int alive = 0;
-
-  	#pragma omp for collapse(2)
-    for (int i = i_d; i <= i_f; i++)
-	    for (int j = j_d; j <= j_f; j++){
-			alive = (cur_img(i-1, j-1) !=0)
-			  + (cur_img(i-1, j) != 0)
-			  + (cur_img(i+1, j-1) != 0)
-			  + (cur_img(i, j-1) != 0)
-			  + (cur_img(i, j+1) != 0)
-			  + (cur_img(i-1, j+1) != 0)
-			  + (cur_img(i+1, j) != 0)
-			  + (cur_img(i+1, j+1) !=0);
-
-			if(cur_img(i,j) != 0){
-			  	if ((alive == 2) || (alive == 3)){
-			    	next_img(i,j) = 0xFFFF00FF;
-			    	test_matrice = 1;
-			  	}
-			  	else
-			    	next_img(i,j) = 0;
-			}
-			else{
-			  	if(alive == 3)
-			    	next_img(i,j) = 0xFFFF00FF;
-			  	else{
-			    	next_img(i,j) = 0;
-			  		test_matrice = 1;
-			  	}
-			}
-		
-
-			alive = 0;
+   		#pragma omp for collapse(2)
+    	for (int i = ret[0]; i <= ret[2]; i++)
+		    for (int j = ret[1]; j <= ret[3]; j++){
+				calcul_vie(i,j);
 			
 	    }
 	}
     
+   	if(test_matrice == 1)
+	    cellule[a][b] = 1;
+	else
+	    cellule[a][b] = 0;
+
   	return 0;
-}
-
-void lancer_jeu_v6 (int i, int j)
-{
-  	int i_d = (i == 0) ? 1 : i * tranche;
-  	int j_d = (j == 0) ? 1 : j * tranche;
-  	int i_f = (i == GRAIN-1) ? DIM-2 : (i+1) * tranche - 1;
-  	int j_f = (j == GRAIN-1) ? DIM-2 : (j+1) * tranche - 1;
-
-  	PRINT_DEBUG ('c', "Descente: bloc(%d,%d) couvre (%d,%d)-(%d,%d)\n", i, j, i_d, j_d, i_f, j_f);
-  
-  	if (jeu_vie_v6 (i_d, j_d, i_f, j_f))
-    	cont = 1;
-
-	if(test_matrice == 1){
-    	cellule[i][j] = 1;
-    }
-    else
-    	cellule[i][j] = 0;
 }
 
 // Renvoie le nombre d'itérations effectuées avant stabilisation, ou 0
@@ -596,7 +388,7 @@ unsigned compute_v6 (unsigned nb_iter)
 	    			&& cellule[i+1][j+1] == 0 )
 	    			continue;
 	    		test_matrice = 0;
-      			lancer_jeu_v6 (i, j);
+      			jeu_vie_v6 (i, j);
     		}
 	}
 	}	
@@ -605,12 +397,42 @@ unsigned compute_v6 (unsigned nb_iter)
   return cont;
 }
 
+//////////////////////////version OMP TASK tuilée
+
+unsigned jeu_vie_v7 (int a, int b)
+{
+	int ret[4];
+	get_tuile(ret, a, b);
+	
+	#pragma omp parallel
+   	{
+
+    	for (int i = ret[0]; i <= ret[2]; i++)
+		    for (int j = ret[1]; j <= ret[3]; j++){
+				calcul_vie(i,j);
+		    }
+	}
+  	return 0;
+}
 
 
+// Renvoie le nombre d'itérations effectuées avant stabilisation, ou 0
 unsigned compute_v7 (unsigned nb_iter)
 { 
-  	return 1;
+  	tranche = DIM / GRAIN;
+
+  	for (unsigned test = 1; test <= nb_iter; test++) {
+  		cont = 0;
+  		for (int i=0; i < GRAIN; i++)
+    		for (int j=0; j < GRAIN; j++){
+      			jeu_vie_seq (i, j);
+    		}
+	}
+   	swap_images ();
+  return cont;
 }
+
+//////////////////////////version OMP TASK optimisé
 
 unsigned compute_v8 (unsigned nb_iter)
 { 
